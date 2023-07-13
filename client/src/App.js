c12b36d71b5be2a924eff326eff8e06bb90b61ae
Vincent Lau <35178680+GalacticKnight@users.noreply.github.com>
2023-07-12 21:25:43 -0400

    second commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';// I have to skip material-ui core. Its
 
 import { useDispatch } from 'react-redux';//this enables us to dispatch an action//we will need to know how to dispatch it. Best used by useEffect
@@ -11,6 +11,8 @@ import useStyles from './styles';
 import memories from './images/memories.png';
 
 const App =()=>{
+    const [currentId, setCurrentId] = useState(0);
+
     const classes = useStyles();//really interesting way to use styles here
     // export default makeStyles(() => ({
     //     appBar: {
@@ -31,27 +33,27 @@ const App =()=>{
     const dispatch = useDispatch();
     useEffect(() => {
         dispatch(getPosts());
-    }, [dispatch]);
+    }, [currentId, dispatch]);
 
     return (
         <Container maxWidth="lg">
-            <AppBar className={classes.appBar} position="static" color="inherit">
-                <Typography className={classes.heading}variant="h2" align="center">Memories</Typography>
-                <img className={classes.image} src={memories} alt="icon" height="200" />
-            </AppBar>
-            <Grow in>
-                <Container>
-                    <Grid container justify="space-between" alignItems="stretch" spacing={3}>
-                        <Grid item xs={12} sm={7}>
-                            <Posts/>
-                        </Grid>
-                        <Grid item xs={12} sm={4}>
-                            <Form/>
-                        </Grid>
-                    </Grid>
-                </Container>
-            </Grow>
-        </Container>
+        <AppBar className={classes.appBar} position="static" color="inherit">
+          <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
+          <img className={classes.image} src={memories} alt="icon" height="60" />
+        </AppBar>
+        <Grow in>
+          <Container>
+            <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+              <Grid item xs={12} sm={7}>
+                <Posts setCurrentId={setCurrentId} />
+              </Grid>
+              <Grid item xs={12} sm={4}>
+                <Form currentId={currentId} setCurrentId={setCurrentId} />
+              </Grid>
+            </Grid>
+          </Container>
+        </Grow>
+      </Container>
     );
 }
 export default App;
